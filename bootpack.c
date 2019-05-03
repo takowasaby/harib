86f483c173b4dd94d5bbd1bcaa0e271f6214d614
祐介 富岡 <tomioka2012@gmail.com>
2019-05-04 03:02:47 +0900

    01d

--- bootpack.c
+++ bootpack.c
@@ -5,10 +5,10 @@ void HariMain(void)
     int i;
     char *p;
 
-    for (i = 0xa0000; i <= 0xaffff; i++)
+    p = (char *) 0xa0000;
+    for (i = 0; i <= 0xffff; i++)
     {
-        p = (char *) i;
-        *p = i & 0x0f;
+        *(p + i) = i & 0x0f;
     }
 
     for (;;)
