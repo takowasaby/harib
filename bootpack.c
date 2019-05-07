718b1268fd55671c64f3c82e8643ae485de6aa43
祐介 富岡 <tomioka2012@gmail.com>
2019-05-07 12:07:57 +0900

    04f

--- bootpack.c
+++ bootpack.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include "bootpack.h"
 
-extern struct KEYBUF keybuf;
+extern struct FIFO8 keyfifo;
+void enable_mouse(void);
+void init_keyboard(void);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-	char s[40], mcursor[256];
+	char s[40], mcursor[256], keybuf[32];
 	int mx, my, i;
 
 	init_gdtidt();
 	init_pic();
 	io_sti();
+	fifo8_init(&keyfifo, 32, keybuf);
+	io_out8(PIC0_IMR, 0xf9);
+	io_out8(PIC1_IMR, 0xef);
+
+	init_keyboard();
 
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -27,29 +34,62 @@ void HariMain(void)
 	putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "King Warthur was a legendary leader");
 	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "King Warthur was a legendary leader");
 
-	io_out8(PIC0_IMR, 0xf9);
-	io_out8(PIC1_IMR, 0xef);
+	enable_mouse();
 
 	for (;;) 
 	{
 		io_cli();
-		if (keybuf.len == 0)
+		if (fifo8_status(&keyfifo) == 0)
 		{
 			io_stihlt();
 		}	
 		else
 		{
-			i = keybuf.data[keybuf.next_r];
-			keybuf.len--;
-			keybuf.next_r++;
-			if (keybuf.next_r == 32)
-			{
-				keybuf.next_r = 0;
-			}
+			i = fifo8_get(&keyfifo);
 			io_sti();
 			sprintf(s, "%02X", i);
 			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
 			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
 		}
 	}
+}
+
+#define PORT_KEYDAT 			0x0060
+#define PORT_KEYSTA 			0x0064
+#define PORT_KEYCMD  			0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE 		0x60
+#define KBC_MODE				0x47
+
+void wait_KBC_sendready(void)
+{
+	for (;;)
+	{
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0)
+		{
+			break;
+		}
+	}
+	return;
+}
+
+void init_keyboard(void)
+{
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return;
+}
+
+#define KEYCMD_SENDTO_MOUSE 	0xd4
+#define MOUSECMD_ENABLE 		0xf4
+
+void enable_mouse(void)
+{
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return;
 }
