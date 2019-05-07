a36410ecdab2867fae1c78595de8200f79aeda0b
祐介 富岡 <tomioka2012@gmail.com>
2019-05-07 11:00:01 +0900

    04b

--- bootpack.c
+++ bootpack.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+extern struct KEYBUF keybuf;
+
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	char s[40], mcursor[256];
-	int mx, my;
+	int mx, my, i;
 
 	init_gdtidt();
 	init_pic();
@@ -28,7 +30,21 @@ void HariMain(void)
 	io_out8(PIC0_IMR, 0xf9);
 	io_out8(PIC1_IMR, 0xef);
 
-	for (;;) {
-		io_hlt();
+	for (;;) 
+	{
+		io_cli();
+		if (keybuf.flag == 0)
+		{
+			io_stihlt();
+		}	
+		else
+		{
+			i = keybuf.data;
+			keybuf.flag = 0;
+			io_sti();
+			sprintf(s, "%02X", i);
+			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+		}
 	}
 }
