bcfeb2cb92a523b0100da203e2977eeae8ce701e
祐介 富岡 <tomioka2012@gmail.com>
2019-05-09 15:03:07 +0900

    06a

--- bootpack.h
+++ bootpack.h
@@ -12,6 +12,9 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 /* naskfunc.nas */
 void io_hlt(void);
 void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
@@ -89,8 +92,6 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 
 /* int.c */
 void init_pic(void);
-void inthandler21(int *esp);
-void inthandler27(int *esp);
 void inthandler2c(int *esp);
 #define PIC0_ICW1      0x0020
 #define PIC0_OCW2      0x0020
@@ -104,3 +105,22 @@ void inthandler2c(int *esp);
 #define PIC1_ICW2      0x00a1
 #define PIC1_ICW3      0x00a1
 #define PIC1_ICW4      0x00a1
+
+/* keyboard.c */
+void inthandler21(int *esp);
+void wait_KBC_sendready(void);
+void init_keyboard(void);
+extern struct FIFO8 keyfifo;
+#define PORT_KEYDAT 			0x0060
+#define PORT_KEYCMD  			0x0064
+
+/* mouse.c */
+struct MOUSE_DEC
+{
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+void inthandler27(int *esp);
+void enable_mouse(struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+extern struct FIFO8 mousefifo;
