8a6e7ff7d6e0c49b55d131ecabb4d2e131b9dabb
祐介 富岡 <tomioka2012@gmail.com>
2019-05-21 21:09:10 +0900

    16c

--- bootpack.c
+++ bootpack.c
@@ -16,6 +16,8 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void console_task(struct SHEET *sheet, unsigned int memtotal);
 int cons_newline(int cursor_y, struct SHEET *sheet);
+void file_readfat(int *fat, unsigned char *img);
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
 
 #define KEYCMD_LED 		0xed
 
@@ -445,11 +447,13 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	int x, y;
 	struct FILEINFO *finfo = (struct MEMMAN *) (ADR_DISKING + 0x002600);
+	int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
 
 	fifo32_init(&task->fifo, 128, fifobuf, task);
 	timer = timer_alloc();
 	timer_init(timer, &task->fifo, 1);
 	timer_settime(timer, 50);
+	file_readfat(fat, (unsigned char *) (ADR_DISKING + 0x000200));
 
 	putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">", 1);
 
@@ -604,20 +608,51 @@ type_next_file:
 						if (x < 224 && finfo[x].name[0] != 0x00)
 						{
 							y = finfo[x].size;
-							p = (char *) (finfo[x].clustno * 512 + 0x003e00 + ADR_DISKING);
+							p = (char *) memman_alloc_4k(memman, finfo[x].size); 
+							file_loadfile(finfo[x].clustno, finfo[x].size, p, fat, (char *) (0x003e00 + ADR_DISKING));
 							cursor_x = 8;
-							for (x = 0; x < y; x++)
+							for (y = 0; y < finfo[x].size; y++)
 							{
-								s[0] = p[x];
+								s[0] = p[y];
 								s[1] = 0;
-								putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
-								cursor_x += 8;
-								if (cursor_x == 8 + 496)
+								if (s[0] == 0x09)
+								{
+									for (;;)
+									{
+										putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+										cursor_x += 8;
+										if (cursor_x == 8 + 496)
+										{
+											cursor_x = 8;
+											cursor_y = cons_newline(cursor_y, sheet);
+										}
+										if (((cursor_x - 8) & 0x1f) == 0)
+										{
+											break;
+										}
+									}
+								}
+								else if (s[0] == 0x0a)
 								{
 									cursor_x = 8;
 									cursor_y = cons_newline(cursor_y, sheet);
 								}
+								else if (s[0] == 0x0d)
+								{
+
+								}
+								else
+								{
+									putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
+									cursor_x += 8;
+									if (cursor_x == 8 + 496)
+									{
+										cursor_x = 8;
+										cursor_y = cons_newline(cursor_y, sheet);
+									}
+								}
 							}
+							memman_free_4k(memman, (int) p, finfo[x].size);
 						}
 						else
 						{
@@ -682,4 +717,40 @@ int cons_newline(int cursor_y, struct SHEET *sheet)
 		sheet_refresh(sheet, 8, 28, 8 + 496, 28 + 304);
 	}
 	return cursor_y;
+}
+
+void file_readfat(int *fat, unsigned char *img)
+{
+	int i, j = 0;
+	for (i = 0; i < 2880; i += 2)
+	{
+		fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;
+		fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;
+		j += 3;
+	}
+	return;
+}
+
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img)
+{
+	int i;
+	for (;;)
+	{
+		if (size <= 512)
+		{
+			for (i = 0; i < size; i++)
+			{
+				buf[i] = img[clustno * 512 + i];
+			}
+			break;
+		}
+		for (i = 0; i < 512; i++)
+		{
+			buf[i] = img[clustno * 512 + i];
+		}
+		size -= 512;
+		buf += 512;
+		clustno = fat[clustno];
+	}
+	return;
 }
