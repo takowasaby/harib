ba093047aafc9a8cda2d67a44b1fb4fd3696f05f
祐介 富岡 <tomioka2012@gmail.com>
2019-05-26 11:23:10 +0900

    21e

--- console.c
+++ console.c
@@ -61,7 +61,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 			}
 			if (i == 3)
 			{
-				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, cons.cur_c, cons.cur_x + 7, cons.cur_c + 15);
+				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
 				cons.cur_c = -1;
 			}
 			if (256 <= i && i <= 511)
@@ -351,7 +351,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 			for (i = 0; i < MAX_SHEETS; i++)
 			{
 				sht = &(shtctl->sheets0[i]);
-				if (sht->flags != 0 && sht->task == task)
+				if ((sht->flags & 0x11) == 0x11 && sht->task == task)
 				{
 					sheet_free(sht);
 				}
@@ -401,6 +401,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	{
 		sht = sheet_alloc(shtctl);
 		sht->task = task;
+		sht->flags |= 0x10;
 		sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
 		make_window8((char *) ebx + ds_base, esi, edi, (char *) ecx + ds_base, 0);
 		sheet_slide(sht, 100, 50);
