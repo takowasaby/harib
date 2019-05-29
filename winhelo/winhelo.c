d63d950c71dcc6dea0fcd925100808de507f9124
祐介 富岡 <tomioka2012@gmail.com>
2019-05-30 03:09:22 +0900

    24g

---
+++ winhelo/winhelo.c
@@ -0,0 +1,17 @@
+#include "apilib.h"
+
+char buf[150 * 50];
+
+void HariMain(void)
+{
+    int win;
+    win = api_openwin(buf, 158, 50, -1, "hello");
+    for (;;)
+    {
+        if (api_getkey(1) == 0x0a)
+        {
+            break;
+        }
+    }
+    api_end();
+}
--- winhelo2/winhelo2.c
+++ winhelo2/winhelo2.c
@@ -1,14 +1,19 @@
 #include "apilib.h"
 
+char buf[150 * 50];
+
 void HariMain(void)
 {
-    char *buf;
     int win;
-
-    api_initmalloc();
-    buf = api_malloc(150 * 50);
     win = api_openwin(buf, 150, 50, -1, "hello");
-    api_boxfillwin(win, 8, 36, 141, 43, 6);
+    api_boxfillwin(win, 8, 36, 141, 43, 3);
     api_putstrwin(win, 28, 28, 0, 12, "hello, world");
+    for (;;)
+    {
+        if (api_getkey(1) == 0x0a)
+        {
+            break;
+        }
+    }
     api_end();
 }
